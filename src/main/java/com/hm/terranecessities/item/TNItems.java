ff10b064692c0c36bfa6e96e27c097a95d70c18e
Notled-Isel <58927333+Notled-Isel@users.noreply.github.com>
2019-12-18 21:21:34 -0800

    First Programming I ever did
    im so good, 99% credit to me
    i am credit to team
    
    dfnagklalkflkdsmngklaewfkdnsklgnawekfjnsdkgnjkewjklsjgklgkjaklfd
    
    I feel bigger
    
    GOOD
    
    i cant read

--- src/main/java/com/hm/terranecessities/item/TNItems.java
+++ src/main/java/com/hm/terranecessities/item/TNItems.java
@@ -6,13 +6,15 @@ import net.minecraft.item.Item;
 public class TNItems {
 	private static String[] moldMetaNames = new String[] { "clay", "ceramic", "fired_copper", "fired_bronze", "fired_bismuth_bronze", "fired_black_bronze" };
 	
-	public static Item clayMoldDoubleKnife;
+	public static Item clayMoldDoubleKnife, clayMoldDoubleHoe;
 	
 	public static void loadItems() {
 		clayMoldDoubleKnife = new TNIPotteryMold().setMetaNames(moldMetaNames).setUnlocalizedName("double_knife_mold");
+		clayMoldDoubleHoe = new TNIPotteryMold().setMetaNames(moldMetaNames).setUnlocalizedName("double_hoe_mold");
 	}
 	
 	public static void registerItems() {
 		GameRegistry.registerItem(clayMoldDoubleKnife, clayMoldDoubleKnife.getUnlocalizedName());
+		GameRegistry.registerItem(clayMoldDoubleHoe, clayMoldDoubleHoe.getUnlocalizedName());
 	}
 }
